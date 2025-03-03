8960ba69fc17d31050f4cef5c885e2545e3b3829
OlivierKouokam <kouokamcarl@gmail.com>
2025-03-04 00:39:09 +0100

    add modules

--- docker-tf/main.tf
+++ docker-tf/main.tf
@@ -21,39 +21,57 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.docker_region
   # access_key = "YOUR-ACCESS-KEY"
   # secret_key = "YOUR-SECRET-KEY"
   shared_credentials_files = ["../.secrets/credentials"]
   profile                  = "default"
 }
 
-module "docker-ec2" {
-  depends_on    = [module.sg, module.keypair]
-  source        = "../modules/ec2"
-  instance_type = "t3.medium"
-  aws_common_tag = {
-    Name = "docker-ec2"
-  }
-  key_name        = module.keypair.key_name
-  security_groups = [module.sg.aws_sg_name]
-  private_key     = module.keypair.private_key
-  user_data_path  = "../scripts/userdata_docker.sh"
+
+module "docker_vpc" {
+  source         = "../modules/vpc"
+  vpc_name       = "docker-vpc"
+  vpc_cidr_block = "10.1.0.0/16"
 }
 
-module "jenkins-docker-ec2" {
-  depends_on    = [module.sg, module.keypair]
-  source        = "../modules/ec2"
-  instance_type = "t3.medium"
-  aws_common_tag = {
-    Name = "jenkins-docker-ec2"
-  }
-  key_name        = module.keypair.key_name
-  security_groups = [module.sg.aws_sg_name]
-  private_key     = module.keypair.private_key
-  user_data_path  = "../scripts/userdata_jenkins.sh"
+module "public_subnet" {
+  source            = "../modules/subnet"
+  vpc_id            = module.docker_vpc.stack_vpc_id
+  subnet_cidr_block = "10.1.1.0/24"
+  subnet_name       = "docker_public_subnet"
+  subnet_AZ         = var.docker_AZ
 }
 
+module "docker_igw" {
+  source   = "../modules/igw"
+  igw_name = "docker_igw"
+  vpc_id   = module.docker_vpc.stack_vpc_id
+}
+
+module "docker_route_table" {
+  source           = "../modules/route_table"
+  vpc_id           = module.docker_vpc.stack_vpc_id
+  route_table_name = "docker_route_table"
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = module.docker_route_table.route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = module.docker_igw.igw_id
+}
+
+resource "aws_route_table_association" "public_subnet_to_docker_igw" {
+  subnet_id      = module.public_subnet.subnet_id
+  route_table_id = module.docker_route_table.route_table_id
+}
+
+module "sg" {
+  source  = "../modules/sg"
+  sg_name = "docker-sg"
+  vpc_id         = module.docker_vpc.stack_vpc_id
+  vpc_cidr_block = ["0.0.0.0/0"]
+}
 
 module "keypair" {
   source           = "../modules/keypair"
@@ -62,20 +80,38 @@ module "keypair" {
 }
 
 
-module "sg" {
-  source  = "../modules/sg"
-  sg_name = "docker-sg"
+module "docker-ec2" {
+  depends_on    = [module.sg, module.keypair]
+  source        = "../modules/ec2"
+  subnet_id     = module.public_subnet.subnet_id
+  instance_type = "t3.medium"
+  aws_common_tag = {
+    Name = "docker-ec2"
+  }
+  key_name        = module.keypair.key_name
+  security_group_ids = [ module.sg.aws_sg_id ]
+  # security_groups = [module.sg.aws_sg_name]
+  private_key     = module.keypair.private_key
+  user_data_path  = "../scripts/userdata_docker.sh"
 }
 
-module "vpc" {
-  source   = "../modules/vpc"
-  vpc_name = "docker-vpc"
+module "docker-docker-ec2" {
+  depends_on    = [module.sg, module.keypair]
+  source        = "../modules/ec2"
+  subnet_id     = module.public_subnet.subnet_id
+  instance_type = "t3.medium"
+  aws_common_tag = {
+    Name = "docker-docker-ec2"
+  }
+  key_name        = module.keypair.key_name
+  security_group_ids = [ module.sg.aws_sg_id ]
+  # security_groups = [module.sg.aws_sg_name]
+  private_key     = module.keypair.private_key
+  user_data_path  = "../scripts/userdata_docker.sh"
 }
-
-
 
 resource "null_resource" "output_metadatas" {
-  depends_on = [module.docker-ec2]
+  depends_on = [module.docker-ec2, ]
   provisioner "local-exec" {
     command = "echo PUBLIC_IP: ${module.docker-ec2.public_ip} - PUBLIC_DNS: ${module.docker-ec2.public_dns}  > docker_ec2.txt"
   }
---
+++ docker-tf/variables.tf
@@ -0,0 +1,14 @@
+variable "static_key_name" {
+  type    = string
+  default = "devops-olivier"
+}
+
+variable "docker_AZ" {
+  type    = string
+  default = "us-east-1a"
+}
+
+variable "docker_region" {
+  type    = string
+  default = "us-east-1"
+}
